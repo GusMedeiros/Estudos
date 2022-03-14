cdcb4560b7523928ba216c18d59d41f70ecc4808
GusMedeiros <96637520+GusMedeiros@users.noreply.github.com>
2022-03-14 14:45:26 -0300

    condicoes - if

---
+++ Fundamentos de C#/Arquivos/Condicoes/Program.cs
@@ -0,0 +1,8 @@
+﻿byte idade = Convert.ToByte((Console.ReadLine()));
+const byte maioridade = 21; 
+if (idade >= maioridade) 
+    Console.WriteLine("Você é maior de idade nos EUA e no Brasil.");
+else if (idade >= 18)
+    Console.WriteLine("Você é menor de idade nos EUA, porém maior de idade no Brasil.");
+else
+    Console.WriteLine("Você é menor de idade tanto no Brasil quanto nos EUA.");
