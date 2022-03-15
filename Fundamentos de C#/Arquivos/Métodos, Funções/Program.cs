ee39593a62d236326fb61f55d38ffb3f9635a644
GusMedeiros <96637520+GusMedeiros@users.noreply.github.com>
2022-03-15 11:18:37 -0300

    Métodos/funções

---
+++ Fundamentos de C#/Arquivos/Métodos, Funções/Program.cs
@@ -0,0 +1,20 @@
+﻿static int Quadrado(int parametro)
+{
+    return parametro * parametro;
+}
+
+Console.WriteLine(Quadrado(2));
+
+static string RetornaNome(string nome, string sobrenome, int? idade = null)
+{
+    return $"{nome} {sobrenome}{(idade==null?null:$" {idade}")}";
+}
+
+Console.WriteLine(RetornaNome("Gustavo", "Medeiros"));
+Console.WriteLine(RetornaNome("Gustavo", "Medeiros", 21));
+
+static string Hoje()
+{
+    return DateTime.Today.ToShortDateString();
+}
+Console.WriteLine($"Hoje é {Hoje()}");
