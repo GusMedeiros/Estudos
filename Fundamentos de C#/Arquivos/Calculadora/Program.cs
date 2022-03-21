46ae2f2d4d8d2192da731c4671dd68050f36714b
GusMedeiros <gustavomedeiros@id.uff.br>
2022-03-21 10:13:02 -0300

    Enumeradores e calculadora

---
+++ Fundamentos de C#/Arquivos/Calculadora/Program.cs
@@ -0,0 +1,163 @@
+﻿void menu(double? resultado)
+{
+    if (resultado == null)
+    {
+        Console.Clear();
+        Console.WriteLine("Bem-vindo à calculadora!!");
+        Console.WriteLine("Selecione a operação desejada:");
+    }
+
+    Console.WriteLine("=================================");
+    Console.WriteLine("Digite:");
+    Console.WriteLine("+ para somar");
+    Console.WriteLine("- para subtrair");
+    Console.WriteLine("* para multiplicar");
+    Console.WriteLine("/ para dividir");
+    Console.WriteLine("^ para potenciar");
+    Console.WriteLine("v para radiciar");
+    Console.WriteLine("S para sair da calculadora");
+    Console.WriteLine("================================");
+    Console.Write("-> ");
+}
+double? somar(double? resultado)
+{
+    double v1;
+    if (resultado == null) // será null quando for a primeira operação feita na execução
+    {
+        Console.WriteLine("Insira o valor 1: ");
+        v1 = double.Parse(Console.ReadLine());
+    }
+    else v1 = (double)resultado; // quando não for a primeira operação, a operação será realizada no resultado anterior.
+
+    Console.WriteLine("Insira o valor 2: ");
+    double v2 = double.Parse(Console.ReadLine());
+
+    resultado = v1 + v2;
+    return resultado;
+}
+double? subtrair(double? resultado)
+{
+    double v1;
+    if (resultado == null) // será null quando for a primeira operação feita na execução
+    {
+        Console.WriteLine("Insira o valor 1: ");
+        v1 = double.Parse(Console.ReadLine());
+    }
+    else v1 = (double)resultado; // quando não for a primeira operação, a operação será realizada no resultado anterior.
+
+    Console.WriteLine("Insira o valor 2: ");
+    double v2 = double.Parse(Console.ReadLine());
+
+    resultado = v1 - v2;
+    return resultado;
+}
+double? multiplicar(double? resultado)
+{
+    double v1;
+    if (resultado == null) // será null quando for a primeira operação feita na execução
+    {
+        Console.WriteLine("Insira o valor 1: ");
+        v1 = double.Parse(Console.ReadLine());
+    }
+    else v1 = (double)resultado; // quando não for a primeira operação, a operação será realizada no resultado anterior.
+
+    Console.WriteLine("Insira o valor 2: ");
+    double v2 = double.Parse(Console.ReadLine());
+
+    resultado = v1 * v2;
+    return resultado;
+}
+double? dividir(double? resultado)
+{
+    double v1;
+    if (resultado == null) // será null quando for a primeira operação feita na execução
+    {
+        Console.WriteLine("Insira o valor 1: ");
+        v1 = double.Parse(Console.ReadLine());
+    }
+    else v1 = (double)resultado; // quando não for a primeira operação, a operação será realizada no resultado anterior.
+
+    Console.WriteLine("Insira o valor 2: ");
+    double v2 = double.Parse(Console.ReadLine());
+    if (v2 == 0)
+    {
+        Console.Clear();
+        Console.WriteLine("ERRO: NÃO É POSSÍVEL DIVIDIR POR ZERO!!");
+        return null;
+    }
+    resultado = v1 / v2;
+    return resultado;
+}
+double? potenciar(double? resultado)
+{
+    double v1;
+    if (resultado == null) // será null quando for a primeira operação feita na execução
+    {
+        Console.WriteLine("Insira o valor 1: ");
+        v1 = double.Parse(Console.ReadLine());
+    }
+    else v1 = (double)resultado; // quando não for a primeira operação, a operação será realizada no resultado anterior.
+
+    Console.WriteLine("Insira o valor 2: ");
+    double v2 = double.Parse(Console.ReadLine());
+
+    resultado = Math.Pow(v1, v2);
+    return resultado;
+}
+double? radiciar(double? resultado)
+{
+    double v1;
+    if (resultado == null) // será null quando for a primeira operação feita na execução
+    {
+        Console.WriteLine("Insira o valor 1: ");
+        v1 = double.Parse(Console.ReadLine());
+    }
+    else v1 = (double)resultado; // quando não for a primeira operação, a operação será realizada no resultado anterior.
+
+    Console.WriteLine("Insira o valor 2: ");
+    double v2 = double.Parse(Console.ReadLine());
+
+    resultado = Math.Pow(v1, 1 / v2);
+    return resultado;
+}
+
+double? resultado = null;
+do
+{
+    menu(resultado);
+    char operacao = Console.ReadKey().KeyChar;
+    Console.WriteLine();
+    switch (operacao)
+    {
+        case '+':
+            resultado = somar(resultado);
+            break;
+        case '-':
+            resultado = subtrair(resultado);
+            break;
+        case '*':
+            resultado = multiplicar(resultado);
+            break;
+        case '/':
+            resultado = dividir(resultado);
+            break;
+        case '^':
+            resultado = potenciar(resultado);
+            break;
+        case 'v':
+            resultado = radiciar(resultado);
+            break;
+        case 's':
+        case 'S':
+            resultado = null;
+            break;
+        default:
+            Console.WriteLine("ERRO: OPERAÇÃO INVÁLIDA!");
+            break;
+    }
+    if (resultado != null)
+        Console.Clear();
+    Console.WriteLine($"{(resultado == null ? "Obrigado por usar a calculadora!!" : "Resultado:")}");
+    Console.WriteLine(resultado);
+} while (resultado != null);
+Console.ReadLine();
---
+++ Fundamentos de C#/Arquivos/Enumeradores/Program.cs
@@ -0,0 +1,56 @@
+﻿Console.WriteLine("Insert the employee's name: ");
+string name = Console.ReadLine();
+Console.WriteLine("Insert the employee's branch: ");
+string input = Console.ReadLine().ToLower();
+input = char.ToUpper(input[0]) + input.Substring(1);
+EBranch branch;
+switch (input)
+{
+    case "Scranton":
+        branch = EBranch.Scranton;
+        break;
+    case "Akron":
+        branch = EBranch.Akron;
+        break;
+    case "Albany":
+        branch = EBranch.Albany;
+        break;
+    case "Nashua":
+        branch = EBranch.Nashua;
+        break;
+    default:
+        branch = EBranch.Nashua;
+        break;
+}
+Console.WriteLine("Insert the employee's salary: ");
+decimal salary = Convert.ToDecimal(Console.ReadLine());
+
+var employee1 = new Employee(name,branch,salary);
+Enum.TryParse(input, out EBranch branch2);
+var employee2 = new Employee(name,branch2,salary);
+Console.WriteLine("You created the following employee:");
+Console.WriteLine($"Name: {employee1.Name} | Branch: {employee1.Branch}, ID: {(int)employee1.Branch} | Salary: {employee1.Salary.ToString("c2")}");
+Console.WriteLine("Test:");
+Console.WriteLine($"Name: {employee2.Name} | Branch: {employee2.Branch}, ID: {(int)employee2.Branch} | Salary: {employee2.Salary.ToString("c2")}");
+
+enum EBranch // enumerator for Dunder Mifflin's branches
+{
+    Scranton = 1,
+    Akron = 2,
+    Albany = 3,
+    Nashua = 4
+}
+
+struct Employee
+{
+    public string Name;
+    public EBranch Branch;
+    public decimal Salary;
+
+    public Employee(string name, EBranch branch, decimal salary)
+    {
+        Name = name;
+        Branch = branch;
+        Salary = salary;
+    }   
+}
