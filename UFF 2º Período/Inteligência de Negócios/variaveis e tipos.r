e6967586218a77f1413a41460f54d9f9ac5f9753
GusMedeiros <96637520+GusMedeiros@users.noreply.github.com>
2022-04-19 18:43:39 -0300

    R básico

--- UFF 2º Período/Inteligência de Negócios/variaveis e tipos.r
+++ UFF 2º Período/Inteligência de Negócios/variaveis e tipos.r
@@ -6,8 +6,8 @@
 
 msg <- "Hello world!" # character / string
 bool <- TRUE # boolean
-numero <- 2 # numeric
-numInt <- 2L # integer 
+numero <- 2 # numeric (float)
+numInt <- 2L # integer
 numComplex <- 2+5i # complex
 stringRaw <- charToRaw(msg) # bytes da string. Como a string é de certa forma um array de caracteres,
 # mostrará os bytes de cada caractere em sequência.
