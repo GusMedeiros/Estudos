cec59a13945b5a829c4e23cef6f33df3547895e8
GusMedeiros <96637520+GusMedeiros@users.noreply.github.com>
2022-05-07 19:09:31 -0300

    melhorando pong

--- UFF 2º Período/Laboratório de Programação de Jogos/pong2.py
+++ UFF 2º Período/Laboratório de Programação de Jogos/pong2.py
@@ -11,20 +11,20 @@ playerspeedpace = 150
 ballspeedpace = 75
 # inicializando bola
 bola = Sprite('bola.jpg')
-bola.set_position(janela.width / 2 - bola.width / 2, janela.height / 2 - bola.height / 2)
 x_orig_bola = janela.width / 2 - bola.width / 2
 y_orig_bola = janela.height / 2 - bola.height / 2
+bola.set_position(x_orig_bola, y_orig_bola)
 velX = (3 if random() < 0.5 else -3)
 velY = (3 if random() < 0.5 else -3)
 # Inicializando jogadores
 # Jogador 1
 jogador1 = Sprite('barra.jpg')
 pontosjogador1 = 0
-jogador1.set_position(25, janela.height / 2 - jogador1.height/2)
+jogador1.set_position(jogador1.width, janela.height / 2 - jogador1.height/2)
 # Jogador 2
 jogador2 = Sprite('barra.jpg')
 pontosjogador2 = 0
-jogador2.set_position(janela.width - 25, janela.height/2 - jogador2.height/2)
+jogador2.set_position(janela.width - 2 * jogador2.width , janela.height/2 - jogador2.height/2)
 
 # Game Loop
 while True:
@@ -46,21 +46,36 @@ while True:
     # updates
     bola.x += velX * pace * ballspeedpace
     bola.y += velY * pace * ballspeedpace
-    if bola.x <= 0:  # Clisão parede esquerda
+    if bola.x + bola.width <= 0:  # Colisão parede esquerda
         bola.set_position(x_orig_bola, y_orig_bola)
+        velX = (3 if random() < 0.5 else -3)
+        velY = (3 if random() < 0.5 else -3)
         pontosjogador2 += 1
-    if bola.x + bola.width >= janela.width:  # Colisão parede direita
+    if bola.x >= janela.width:  # Colisão parede direita
         bola.set_position(x_orig_bola, y_orig_bola)
+        velX = (3 if random() < 0.5 else -3)
+        velY = (3 if random() < 0.5 else -3)
         pontosjogador1 += 1
     if bola.y <= 0 or bola.y + bola.height >= janela.height:  # Colisão com teto ou chão
-        velY *= -1
-        bola.x += velY * pace * ballspeedpace  # desfaz o movimento que causou a colisão
+        velY *= -1 
+        bola.y += velY * pace * ballspeedpace  # desfaz o movimento que causou a colisão
     if bola.collided(jogador1):
-        velX *= -1
-        bola.x = jogador1.x + bola.width # desfaz a colisão teleportando a bola
+        if bola.x - velX * pace * ballspeedpace < jogador1.x + jogador1.width:  # se bateu nas pontas da barra
+            velY *= -1
+            bola.y = jogador1.y - bola.height if bola.y < jogador1.y + jogador1.height / 2 else jogador1.y + jogador1.height 
+            # bola.y += velY * pace * ballspeedpace
+        else:
+            velX *= -1
+            bola.x += velX * pace * ballspeedpace
     if bola.collided(jogador2):
-        velX *= -1
-        bola.x = jogador2.x - bola.width  # desfaz a colisão
+        if bola.x + bola.width - velX * pace * ballspeedpace > jogador2.x:  # se bateu nas pontas da barra
+            velY *= -1
+            bola.y = jogador2.y - bola.height if bola.y < jogador2.y + jogador2.height / 2 else jogador2.y + jogador2.height 
+            # bola.y += velY * pace * ballspeedpace         
+        else:
+            velX *= -1
+            bola.x += velX * pace * ballspeedpace
+
     # desenhar
     fundo.draw()
     jogador1.draw()
---
+++ UFF 2º Período/Laboratório de Programação de Jogos/tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+bola.set_position(x_orig_bola, y_orig_bola)
+        velX = (3 if random() < 0.5 else -3)
+        velY = (3 if random() < 0.5 else -3)
